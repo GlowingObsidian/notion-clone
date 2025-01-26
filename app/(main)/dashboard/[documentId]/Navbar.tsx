caa3d597f0e5b4afb24f4e5e9a2e271cf4fc818e
Shreyan Dey <deyshreyan24@outlook.com>
2025-01-26 20:07:33 +0530

    Implemented publish page

--- app/(main)/dashboard/[documentId]/Navbar.tsx
+++ app/(main)/dashboard/[documentId]/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Banner from "@/app/components/Banner";
 import Menu from "@/app/components/Menu";
+import Publish from "@/app/components/Publish";
 import Title from "@/app/components/Title";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -43,9 +44,12 @@ function Navbar({
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
-          <div className="flex items-center gap-x-2">
-            {!document.isArchived && <Menu documentId={document._id} />}
-          </div>
+          {!document.isArchived && (
+            <div className="flex items-center gap-x-2">
+              <Publish initialData={document} />
+              <Menu documentId={document._id} />
+            </div>
+          )}
         </div>
       </div>
     </>
